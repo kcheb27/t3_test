9bbe71127e7afc07057d4a944be73f27b134c816
kcheb27 <maykevin.chen@gmail.com>
2023-08-12 14:44:20 -0400

    retry

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 import { SignInButton} from "@clerk/nextjs";
-import { SignOutButton} from "@clerk/nextjs";
+
 
 const CreatePostWizard = () =>{
   const {user} = useUser();
